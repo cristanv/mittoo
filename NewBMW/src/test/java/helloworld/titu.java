78cf266faa9ead0bc44ec6b4d5a812d5ac21727e
Admin <Admin@DESKTOP-46E6TOF>
2022-08-07 19:56:21 +0530

    done bro

---
+++ NewBMW/src/test/java/helloworld/titu.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+import org.testng.annotations.Test;
+
+public class titu {
+	
+	@Test(groups = "smoke")
+	public void hellodeepa()
+	{
+		System.out.println("how are you222222");
+	}
+	@Test(groups = "regration")
+	public void haveyou()
+	{
+		System.out.println("have coffee2222");
+	}
+}
